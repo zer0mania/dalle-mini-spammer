f883d6f2a9d36a20546073eae91a38df217ffe49
zer0mania <48740591+zer0mania@users.noreply.github.com>
2022-06-16 23:44:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ import os
 import requests
 import re
 
+print('''
+██████╗  █████╗ ██╗     ██╗      ███████╗    ███╗   ███╗██╗███╗   ██╗██╗    
+██╔══██╗██╔══██╗██║     ██║      ██╔════╝    ████╗ ████║██║████╗  ██║██║    
+██║  ██║███████║██║     ██║█████╗█████╗      ██╔████╔██║██║██╔██╗ ██║██║    
+██║  ██║██╔══██║██║     ██║╚════╝██╔══╝      ██║╚██╔╝██║██║██║╚██╗██║██║    
+██████╔╝██║  ██║███████╗███████╗ ███████╗    ██║ ╚═╝ ██║██║██║ ╚████║██║    
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══════╝    ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝    
+                                                            @zer0mania                 
+
+This AI can generate images based on prompts you give, it can take 1.5-3 minutes to generate and makes 9 256x256 images and saves it.
+''')
+
 while True:
     prompt = input("Enter prompt: ")
     print("Generating..")
     while True:
         url = "https://bf.dallemini.ai:443/generate"
-        headers = {"Sec-Ch-Ua": "\"-Not.A/Brand\";v=\"8\", \"Chromium\";v=\"102\"", "Accept": "application/json", "Content-Type": "application/json", "Sec-Ch-Ua-Mobile": "?0", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36", "Sec-Ch-Ua-Platform": "\"Windows\"", "Origin": "https://hf.space", "Sec-Fetch-Site": "cross-site", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://hf.space/", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
         json = {"prompt": f"{prompt}"}
         tic = time.perf_counter()
-        response = requests.post(url, headers=headers, json=json)
+        response = requests.post(url, json=json)
         if "mega-1:v16" in response.text:
             break
         print(response.text)
